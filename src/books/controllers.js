d6fe2666ceb6e9927086e5f9b75252a5a0d056d5
Robert Zdeb <robinio123@gmail.com>
2023-03-01 12:09:13 +0000

    create addbook method

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,7 +1,39 @@
+const Book = require ("./module")
+
 const addBook = async (req, res) => {
-    res.status(201).json({
-        message: "hello boogie"
-    })
+    try {
+            // const book = await Book.create( {
+            //     title: req.body.title,
+            //     author: req.body.author,
+            //     genre: req.body.genre,
+            // });
+
+            const book = await Book.create(req.body);
+            console.log(book);
+
+            // const successResponse = {
+            //     message: "success",
+            //     newBook: book,
+            // }
+
+            // res.status(201).json(successResponse);
+            
+            res.status(201).json({ message: "success", newBook: book});
+
+
+    } catch (error) {
+        console.log(error);
+
+        // const errorResponse = {
+        //     errorMessage: error.message,
+        //     error: error,
+        // };
+
+        // res.status(501).json(errorResponse);
+
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+    
 }
 
 module.exports = {
