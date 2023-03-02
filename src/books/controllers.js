810ce393ecbbd09ca66bb6d5dc7a6dbbbd481cf9
Robert Zdeb <robinio123@gmail.com>
2023-03-02 11:48:54 +0000

    finished CRUD operations

--- src/books/controllers.js
+++ src/books/controllers.js
@@ -1,7 +1,94 @@
-const Book = require ("./module")
+const Book = require ("./module");
+
+const getAllBooks = async (req, res) => {
+
+    try {
+
+    const read = await Book.findAll();
+
+    res.status(201).json({
+        message: "success",
+        read: read,
+    });
+
+    } catch (er) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+const getSingleBookByTitle = async (req, res) => {
+
+    try {
+        const read = await Book.findOne( {
+            where: { title: req.params.title }
+        });
+
+        res.status(201).json({
+            message: "success",
+            book: read,
+        });
+        
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+
+    }
+}
+
+const getAllBooksByGenre = async (req, res) => {
+
+    try {
+
+    const read = await Book.findAll({
+        where: {
+            genre: req.body.genre,
+        }
+    });
+
+    res.status(201).json({
+        message: "success",
+        read: read,
+    });
+
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+const updateBook = async (req, res) => {
+    console.log(req.body);
+    try {
+
+        // const filterObj = { title: req.body.title }
+        // const updateObj = { [req.body.updateKey]: req.body.updateValue}
+
+        
+        const updateBook = await Book.update({ 
+            // genre: req.body.newGenre 
+
+            [req.body.updateKey]: req.body.updateValue },             
+            { where: { title: req.body.title }            
+            
+            // { updateObj },
+            // {
+            // where:  { filterObj },              
+            // },
+
+        })
+        console.log(updateBook);
+
+        res.status(201).json({
+            message: "success",
+            updateBook: updateBook,
+        });
+
+    } catch (error) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
 
 const addBook = async (req, res) => {
     try {
+
             // const book = await Book.create( {
             //     title: req.body.title,
             //     author: req.body.author,
@@ -15,9 +102,8 @@ const addBook = async (req, res) => {
             //     message: "success",
             //     newBook: book,
             // }
-
             // res.status(201).json(successResponse);
-            
+
             res.status(201).json({ message: "success", newBook: book});
 
 
@@ -32,10 +118,51 @@ const addBook = async (req, res) => {
         // res.status(501).json(errorResponse);
 
         res.status(501).json({ errorMessage: error.message, error: error });
-    }
-    
+    }    
 }
+
+const deleteAllBooks = async (req, res) => {
+
+    try {
+
+    const deleteAll = await Book.destroy();
+
+    res.status(201).json({
+        message: "success",
+        deleteAll: deleteAll,
+    });
+
+    } catch (er) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+const deleteBook = async (req, res) => {
+
+    try {
+
+    const deleteBook = await Book.destroy( {
+        where: { title: req.body.title }
+    });
+
+    res.status(201).json({
+        message: "success",
+        deleteBook: deleteBook,
+    });
+
+    } catch (er) {
+        res.status(501).json({ errorMessage: error.message, error: error });
+    }
+}
+
+
 
 module.exports = {
     addBook,
+    getAllBooks,
+    getAllBooksByGenre,
+    getSingleBookByTitle,
+    updateBook,
+    deleteAllBooks,
+    deleteBook,
 }
--- src/books/routes.js
+++ src/books/routes.js
@@ -2,8 +2,15 @@ const { Router } = require("express");
 
 const bookRouter = Router();
 
-const { addBook } = require("./controllers");
+const { getSingleBookByTitle, addBook, getAllBooks, getAllBooksByGenre, updateBook, deleteAllBooks, deleteBook } = require("./controllers");
 
 bookRouter.post("/books/addbook", addBook);
+bookRouter.get("/books/getallbooks", getAllBooks);
+bookRouter.get("/books/getbook/:title", getSingleBookByTitle);
+bookRouter.get("/books/getallbooksbygenre", getAllBooksByGenre);
+bookRouter.put("/books/updatebook", updateBook);
+bookRouter.delete("/books/deleteallbooks", deleteAllBooks);
+bookRouter.delete("/books/deletebook", deleteBook);
+
 
 module.exports = bookRouter;
